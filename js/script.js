1aca15df7a054a21ab9fa4d94c66ca12870b3851
Imana Malia Kondou <missimana@gmail.com>
2025-05-27 22:54:47 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
+ function konversiSuhu() {
+     const suhu = parseFloat(document.getElementById("suhu").value);
+     const satuan = document.getElementById("satuan").value;
+     let hasil = "";
 
-// Get the modal
-var modal = document.getElementById('id01');
+     if (isNaN(suhu)) {
+         hasil = "Masukkan angka suhu yang valid.";
+     } else {
+         let c, f;
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+         switch (satuan) {
+             case "celsius":
+                 c = suhu;
+                 f = (suhu * 9 / 5) + 32;
+                 //  k = suhu + 273.15;
+                 break;
+             case "fahrenheit":
+                 c = (suhu - 32) * 5 / 9;
+                 f = suhu;
+                 //  k = c + 273.15;
+                 break;
+             case "kelvin":
+                 c = suhu - 273.15;
+                 f = (c * 9 / 5) + 32;
+                 //  k = suhu;
+                 break;
+         }
+
+         hasil = `
+          <strong>Hasil Konversi:</strong><br>
+          Celsius: ${c.toFixed(2)} °C<br>
+          Fahrenheit: ${f.toFixed(2)} °F<br>
+          Kelvin: ${k.toFixed(2)} K
+        `;
+     }
+
+     document.getElementById("hasil").innerHTML = hasil;
+ }
